f6b8da295bb0b3a4a69302ee4594391a9a624626
PurePerson <pure@etik.com>
2026-04-27 01:25:25 +0300

    Update XOS_sweet.mk
    Porting...

--- XOS_sweet.mk
+++ XOS_sweet.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/xos/config/common.mk)
+$(call inherit-product, $(CUSTOM_VENDOR_DIR)/config/common.mk)
 
 PRODUCT_NAME := XOS_sweet
 PRODUCT_DEVICE := sweet
