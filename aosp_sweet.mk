6ae99e00ec403ef6719207ac57ce088334da98a5
PurePerson <pure@etik.com>
2026-04-29 11:30:21 +0300

    Update aosp_sweet.mk
    A big mistake has been fixed.

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, $(CUSTOM_VENDOR_DIR)/config/common.mk)
+$(call inherit-product, $(CUSTOM_VENDOR_DIR)/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
